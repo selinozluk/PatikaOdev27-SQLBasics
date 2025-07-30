89d9c6f5ee63e87c47ef731fa5fe389cf1ce6856
selinozluk <selinozluk36@gmail.com>
2025-07-30 09:49:51 +0300

    Add SQL11.sql with UNION, INTERSECT, and EXCEPT (and ALL versions) for dvdrental

---
+++ SQL11.sql
@@ -0,0 +1,30 @@
+-- 1. actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sırala
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer;
+
+-- 2. actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sırala
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
+
+-- 3. actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sırala
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
+
+-- 4. İlk 3 sorguyu tekrar eden veriler için de yap
+-- UNION ALL (tekrar eden first_name değerleri)
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer;
+
+-- INTERSECT ALL (kaç tane tekrar eden ortak veriler varsa)
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+
+-- EXCEPT ALL (actorda olup customerda olmayanların tekrarı)
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer;
