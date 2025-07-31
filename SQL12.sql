1b9f909924d4be305f49cdc9b62b9cf145e0e8ff
selinozluk <selinozluk36@gmail.com>
2025-07-31 23:20:51 +0300

    Add SQL12.sql with dvdrental average length, max/min rates and top customers queries

---
+++ SQL12.sql
@@ -0,0 +1,24 @@
+-- 1. film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film var
+SELECT COUNT(*) 
+FROM film 
+WHERE length > (SELECT AVG(length) FROM film);
+
+
+-- 2. film tablosunda en yüksek rental_rate değerine sahip kaç tane film var
+SELECT COUNT(*) 
+FROM film 
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+
+-- 3. film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sırala
+SELECT *
+FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+
+-- 4. payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sırala
+SELECT customer_id, COUNT(*) AS payment_count
+FROM payment
+GROUP BY customer_id
+ORDER BY payment_count DESC;
